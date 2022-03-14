d04610bb9b461273965acf31bc038f3e33ce9db8
Yong Xian Pang <yongxianpang@Yongs-iMac.local>
2022-03-14 23:44:14 +0800

    first commit

---
+++ handler.js
@@ -0,0 +1,96 @@
+const todosCreate = require('./todos-create.js');
+const todosReadAll = require('./todos-read-all.js');
+const todosReadOne = require('./todos-read-one.js');
+const todosUpdate = require('./todos-update.js');
+const todosDelete = require('./todos-delete.js');
+const todosUpdateStatusTrue = require('./todos-updateStatustoTrue.js');
+const todosUpdateStatusFalse = require('./todos-updateStatustoFalse.js');
+const todosSetDueDate = require('./todos-setDueDate.js');
+
+module.exports.create = (event, context, callback) => {
+  todosCreate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.readAll = (event, context, callback) => {
+  todosReadAll(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.readOne = (event, context, callback) => {
+  todosReadOne(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.update = (event, context, callback) => {
+  todosUpdate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.delete = (event, context, callback) => {
+  todosDelete(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.updateStatustoTrue = (event, context, callback) => {
+  todosUpdateStatusTrue(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.updateStatustoFalse = (event, context, callback) => {
+  todosUpdateStatusFalse(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.setDueDate = (event, context, callback) => {
+  todosSetDueDate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
---
+++ todos-updateStatustoTrue.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports = (event, callback) => {
+  const data = JSON.parse(event.body);
+
+  data.id = event.pathParameters.id;
+  data.updatedAt = new Date().getTime();
+  data.isDone = "true";
+
+  const params = {
+    TableName : 'todos',
+    Item: data
+  };
+
+  return dynamoDb.put(params, (error, data) => {
+    if (error) {
+      callback(error);
+    }
+    callback(error, params.Item);
+  });
+};
